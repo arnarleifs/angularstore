dc51cfecf377cfdd89f424e609946e819bd8d99a
Arnar <arnarleifs@github.com>
2016-03-13 14:21:47 +0000

    100 CC for ProductTabController

--- src/components/product-tabs/ProductTabController.spec.js
+++ src/components/product-tabs/ProductTabController.spec.js
@@ -5,6 +5,18 @@ describe("ProductTabController", function () {
 
 	var ProductTabController, scope;
 
+	var mockProduct = {
+		id: 1,
+		product: {
+			id: 1,
+			name: "productName",
+			price: "price",
+			quantitySold: "quantitySold",
+			quantityInStock: "quantityInStock",
+			imagePath: "path"
+		}
+	};
+
 	var mockHttpPromise = function(condition, data) {
 		return {
 			success: function(fn) {
@@ -36,13 +48,13 @@ describe("ProductTabController", function () {
 
 	var mockResourceTrue = {
 		addSellerProduct: function () {
-			return mockHttpPromise(true, null);
+			return mockHttpPromise(true, mockProduct);
 		},
 		getSellerProducts: function () {
-			return mockHttpPromise(true, null);
+			return mockHttpPromise(true, mockProduct);
 		},
 		updateSellerProduct: function () {
-			return mockHttpPromise(true, null);
+			return mockHttpPromise(true, mockProduct);
 		}
 	};
 
@@ -59,12 +71,55 @@ describe("ProductTabController", function () {
 			ProductTabController = $controller("ProductTabController", {
 				$scope: scope,
 				$mdDialog: mockDialog,
-				AppResource: mockResourceFalse
+				AppResource: mockResourceFalse,
+				$mdToast: mockDialog
 			});
 		}));
 
 		it('should open edit product dialog', function () {
 			scope.editProduct();
+			expect(mockDialog.show).toHaveBeenCalled();
+		});
+
+		it('should get error when retrieving seller products', function () {
+			expect(scope.error).toBe(true);
+		});
+
+		it('should show failed toast when adding to product list', function () {
+			scope.$emit('addToProductList', null);
+			expect(mockDialog.show).toHaveBeenCalled();
+		});
+
+		it('should show failed toast when editing product', function () {
+			scope.$emit('editProduct', null);
+			expect(mockDialog.show).toHaveBeenCalled();
+		});
+	});
+
+	describe("ProductTabController success", function () {
+		beforeEach(inject(function ($rootScope, $controller) {
+			scope = $rootScope.$new();
+			spyOn(mockDialog, "show");
+			ProductTabController = $controller("ProductTabController", {
+				$scope: scope,
+				$mdDialog: mockDialog,
+				AppResource: mockResourceTrue,
+				$mdToast: mockDialog
+			});
+			scope.productList = [];
+		}));
+
+		it('should retrieve seller products', function () {
+			expect(scope.error).toBe(false);
+		});
+
+		it('should show newly added product when adding to product list', function () {
+			scope.$emit('addToProductList', mockProduct);
+			expect(scope.productList).toContain(mockProduct);
+		});
+
+		it('should show newly edited seller when editing product', function () {
+			scope.$emit('editProduct', mockProduct);
 			expect(mockDialog.show).toHaveBeenCalled();
 		});
 	});
